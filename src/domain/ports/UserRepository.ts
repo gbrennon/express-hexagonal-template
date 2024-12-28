2ff20058f9289e7407e6a99505148e10b23f4119
Glauber Brennon <glauberbrennon@gmail.com>
2024-12-28 15:54:50 -0300

    Fix on findByEmail

--- src/domain/ports/UserRepository.ts
+++ src/domain/ports/UserRepository.ts
@@ -1,6 +1,6 @@
 import { User } from '@domain/entities/User';
 
 export interface UserRepository {
-  findByEmail(email: string): Promise<User>;
+  findByEmail(email: string): Promise<User | null>;
   save(user: User): Promise<void>;
 }
